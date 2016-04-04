f4d67e6e66d04bf993d5e87504ec5fc4409fa2eb
Nicoara Talpes <nicoaratalpes@gmail.com>
2016-04-04 21:25:21 +0300

    added button functionality

--- interactiveStory/ViewController.swift
+++ interactiveStory/ViewController.swift
@@ -13,9 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "startAdventure" {
+            if let pageController = segue.destinationViewController as? PageController {
+                pageController.page = Adventure.story
+            }
+        }
+    }
 
 }
 
